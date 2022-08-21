513b990665ebf1337cdfd4d5ed78bcb501e2bd3e
majharul224 <majharulislam2225@gmail.com>
2022-08-21 23:56:47 +0600

    add a baperbanksetup

--- js/withdraw.js
+++ js/withdraw.js
@@ -2,20 +2,35 @@ document.getElementById("btn-withdraw").addEventListener('click', function () {
     const withdrawField = document.getElementById("withdraw-field");
     const withdrawAmountString = withdrawField.value;
     const withdrawAmount = parseFloat(withdrawAmountString);
-    console.log(withdrawAmount)
+    // console.log(withdrawAmount)
+    withdrawField.value = " ";
+    if(isNaN(withdrawAmount)){
+        alert("please valide numver")
+        return;
+    }
 
     const withdrawTotalElement = document.getElementById("withdraw-total")
     const previesWithdrawTotalString = withdrawTotalElement.innerText;
     const previesWithdrawTotal = parseFloat(previesWithdrawTotalString);
-    const carrentWithdrawTotal = previesWithdrawTotal + withdrawAmount;
-    withdrawTotalElement.innerText = carrentWithdrawTotal;
+  /*   const carrentWithdrawTotal = previesWithdrawTotal + withdrawAmount;
+    withdrawTotalElement.innerText = carrentWithdrawTotal; */
 
     const balanceTotalElement = document.getElementById("balance-total")
     const previesBalanceTotalString = balanceTotalElement.innerText;
     const previesBalanceTotal = parseFloat(previesBalanceTotalString)
     // console.log(previesBalanceTotal)
 
+    
+if( withdrawAmount > previesBalanceTotal){
+    alert("baper banka taka ni")
+    return;
+}
+
+const carrentWithdrawTotal = previesWithdrawTotal + withdrawAmount;
+withdrawTotalElement.innerText = carrentWithdrawTotal;
+
+
     const newTotalBalance = previesBalanceTotal - withdrawAmount;
     balanceTotalElement.innerText = newTotalBalance;
-withdrawField.value = " ";
+// withdrawField.value = " ";
 })
